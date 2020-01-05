444eece629e26aeb87492f854d569dbbf2bda5ac
ohil17yo36 <ohileshwar17@gmail.com>
2020-01-04 23:03:30 -0800

    Code changes
    Signed-off-by: ohil17yo36 <ohileshwar17@gmail.com>

--- rate-limiter/src/test/java/rate/limiter/RateLimiterBatchTest.java
+++ rate-limiter/src/test/java/rate/limiter/RateLimiterBatchTest.java
@@ -1,14 +1,19 @@
 package rate.limiter;
 
+import metric.PerformanceMetric;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class RateLimiterBatchTest {
 
+    private static double ERROR_THRESHOLD;
+
     private RateLimitTester rateLimitTester;
 
     @Before
     public void init() {
+        ERROR_THRESHOLD = 0.1;
         rateLimitTester = new RateLimitTester();
     }
 
@@ -16,14 +21,14 @@ public class RateLimiterBatchTest {
     public void firstTest() throws InterruptedException {
         AbstractRateLimiter rl = new TokenBucketRateLimiter(20);
         Thread thread = new Thread(() -> {
-            rateLimitTester.test(rl, 10, 1);
-            rateLimitTester.test(rl, 20, 2);
-            rateLimitTester.test(rl,50, 5);
-            rateLimitTester.test(rl,100, 10);
-            rateLimitTester.test(rl,200, 20);
-            rateLimitTester.test(rl,250, 25);
-            rateLimitTester.test(rl,500, 50);
-            rateLimitTester.test(rl,1000, 100);
+            PerformanceMetric performanceMetric = rateLimitTester.test(rl, 10, 1);
+            Assert.assertTrue(1 - performanceMetric.getHitRate() <= ERROR_THRESHOLD);
+            performanceMetric = rateLimitTester.test(rl, 20, 2);
+            Assert.assertTrue(2 - performanceMetric.getHitRate() <= ERROR_THRESHOLD);
+            performanceMetric = rateLimitTester.test(rl,50, 5);
+            Assert.assertTrue(5 - performanceMetric.getHitRate() <= ERROR_THRESHOLD);
+            performanceMetric = rateLimitTester.test(rl,100, 10);
+            Assert.assertTrue(10 - performanceMetric.getHitRate() <= ERROR_THRESHOLD);
         });
 
         thread.start();
